35246b58981606e1e12a0120e974b08e9743433d
aleksandr-krykun <sasha_krikyn97@ukr.net>
2022-03-24 21:21:48 -0500

    fix issue with all green images

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import cv2
 import numpy as np
 
-img_rdb = cv2.imread('find.jpeg')
-img_gray = cv2.cvtColor(img_rdb, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.imread('find.jpeg')
+img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 target_object = cv2.imread('target.png', 0)
 
-width, height = target_object.shape
+width, height = target_object.shape[::-1]
 
-result = cv2.matchTemplate(img_gray, target_object, cv2.TM_CCORR_NORMED)
-threshold = 0.6  # best value what I found for now
+result = cv2.matchTemplate(img_gray, target_object, cv2.TM_CCOEFF_NORMED)
+threshold = 0.6
 
-loc = np.where(result >= threshold)
+loc = np.where( result >= threshold)
 for pt in zip(*loc[::-1]):
-    cv2.rectangle(img_rdb, pt, (pt[0] + width, pt[1] + height), (0, 255, 0), 2)
+    cv2.rectangle(img_rgb, pt, (pt[0] + width, pt[1] + height), (0,255,0), 2)
 
-cv2.imwrite('result.png', img_rdb)
+cv2.imwrite('result.png', img_rgb)
